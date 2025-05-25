17688f01b4eff67b223742a0a4d344625afb114a
farishamy16 <chibi.hamy@gmail.com>
2025-05-25 23:39:42 +0800

    styling: icon & viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,17 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Sidebar } from "@/components/ui/Sidebar";
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Math Game",
   description: "Interactive math game for practicing rounding numbers",
+};
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({
